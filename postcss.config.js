adf1fe3ba26f65a44d58f29e0d0b818185767152
alberto-mac <pascu216@gmail.com>
2022-09-14 20:30:20 +0200

    Fix style import with TailwindCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,15 @@
 // Configuration reference:
 // https://dev.to/vinbak/adding-tailwindcss-to-your-rails-project-with-postcss-7-compatibility-2o81
+// Learn more about plugin needs:
+// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
   plugins: [
-    // Imports TailwindCSS
-    require('tailwindcss')('./config/base.config.js'),
     // Easy importing of local, node or web CSS files
+    // (!) SET THIS PLUGIN ALWAYS FIRST
     // https://github.com/postcss/postcss-import
     require('postcss-import'),
+    // Imports TailwindCSS
+    require('tailwindcss')('./config/tailwind.config.js'),
     // Fixes bugs associated to flexboxes in different web browsers
     // https://www.npmjs.com/package/postcss-flexbugs-fixes
     require('postcss-flexbugs-fixes'),
