81b13066c1ae47c337ad32bde27b29066f284cbf
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2022-10-07 17:34:07 +0200

    Updated dependencies and Gemfile.lock

--- test/factories/documents.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  # Base Factory
-  factory :document do
-    name { "Post about something" }
-    description { "This is a random post about something" }
-    status { 0 }
-  end
-end
